e3e26d15520625c93bb406a240719a77c422ddcf
gvernard <gvernard@astro.rug.nl>
2021-05-14 19:01:26 +0200

    Modified the docker files to force a part of the builds not to use cache.

--- dockerfiles/docker_auto.sh
+++ dockerfiles/docker_auto.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cp Dockerfile_full Dockerfile
-docker build -t gvernard/molet:development . --network=host
+docker build --build-arg CACHE_DATE="$(date)" -t gvernard/molet:development . --network=host
 cp Dockerfile_production Dockerfile
-docker build -t gvernard/molet:production . --network=host
+docker build --build-arg CACHE_DATE="$(date)" -t gvernard/molet:production . --network=host
 cp Dockerfile_min Dockerfile
 docker build -t gvernard/molet:min . --network=host
 docker push gvernard/molet:min
