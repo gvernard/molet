f0d763b091ad7c137f44d83093d1338f115876ff
gvernard <gvernard@astro.rug.nl>
2022-05-26 18:55:41 +0200

    Working version with the install script. Need to fully move to autotools in the future.

--- install_third_party/install.sh
+++ install_third_party/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+WITH_GPU=no
+MAP_PATH=/home/giorgos/unbacked_up_data/gerlumph_mirror/
 
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]:-$0}"; )" &> /dev/null && pwd 2> /dev/null; )";
@@ -18,7 +20,8 @@ wget ftp://ftp.fftw.org/pub/fftw/fftw-3.3.8.tar.gz \
     &&	cd fftw \
     &&	./configure --prefix=$LIBDIR/fftw --enable-shared \
     &&	make CFLAGS=-fPIC \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
 # Install cfitsio
 wget http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz \
@@ -27,15 +30,17 @@ wget http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz \
     &&	cd cfitsio \
     &&	./configure --prefix=$LIBDIR/cfitsio \
     &&	make \
-    &&	make install
-
+    &&	make install \
+    &&  cd $SRCDIR
+	
 # Install CCfits
 wget https://heasarc.gsfc.nasa.gov/fitsio/CCfits-2.5/CCfits-2.5.tar.gz \
     &&	tar -xvf CCfits-2.5.tar.gz \
     &&	cd CCfits \
     &&	./configure --prefix=$LIBDIR/CCfits --with-cfitsio=$LIBDIR/cfitsio \
     &&	make \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
 # Install libpng
 wget --no-check-certificate https://sourceforge.net/projects/libpng/files/libpng16/1.6.37/libpng-1.6.37.tar.gz \
@@ -43,7 +48,8 @@ wget --no-check-certificate https://sourceforge.net/projects/libpng/files/libpng
     &&	cd libpng-1.6.37 \
     &&	./configure --prefix=$LIBDIR/libpng \
     &&	make check \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
 # Install gmp
 wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz \
@@ -51,7 +57,8 @@ wget https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz \
     &&	cd gmp-6.1.2 \
     &&	./configure --prefix=$LIBDIR/gmp \
     &&	make \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
 # Install mpfr
 wget http://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.gz \
@@ -59,14 +66,16 @@ wget http://www.mpfr.org/mpfr-4.0.2/mpfr-4.0.2.tar.gz \
     &&	cd mpfr-4.0.2 \
     &&	./configure --with-gmp=$LIBDIR/gmp --prefix=$LIBDIR/mpfr \
     &&	make \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
 # Install boost
 wget https://boostorg.jfrog.io/artifactory/main/release/1.66.0/source/boost_1_66_0.tar.gz \
     &&	tar -xvf boost_1_66_0.tar.gz \
     &&	cd boost_1_66_0 \
     &&	./bootstrap.sh --with-libraries=thread \
-    &&	./b2 --prefix=$LIBDIR/boost install
+    &&	./b2 --prefix=$LIBDIR/boost install \
+    &&  cd $SRCDIR
 
 # Install cmake
 wget https://github.com/Kitware/CMake/archive/refs/tags/v3.15.5.tar.gz \
@@ -74,7 +83,8 @@ wget https://github.com/Kitware/CMake/archive/refs/tags/v3.15.5.tar.gz \
     &&  cd CMake-3.15.5 \
     &&  ./bootstrap --prefix=$LIBDIR/cmake \
     &&	make \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
 # Install CGAL
 wget https://github.com/CGAL/cgal/archive/releases/CGAL-4.11.2.tar.gz \
@@ -91,7 +101,8 @@ wget https://github.com/CGAL/cgal/archive/releases/CGAL-4.11.2.tar.gz \
 	    -DCMAKE_INSTALL_PREFIX=$LIBDIR/CGAL \
 	    .. \
     &&	make \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
 # Install jsoncpp
 git clone https://github.com/open-source-parsers/jsoncpp.git \
@@ -106,22 +117,45 @@ git clone https://github.com/open-source-parsers/jsoncpp.git \
 	    -G "Unix Makefiles" \
 	    .. \
     &&	make \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
-# Install jq and libsqlite3 necessary to run MOLET
+# Install jq
 wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz \
     &&	tar -xvf jq-1.5.tar.gz \
     &&	cd jq-1.5 \
     &&  autoreconf -i \
     && ./configure --disable-maintainer-mode --prefix=$LIBDIR/jq \
     &&	make \
-    &&	make install
+    &&	make install \
+    &&  cd $SRCDIR
 
+# Install sqlite3
 wget https://www.sqlite.org/2022/sqlite-autoconf-3380500.tar.gz \
     &&	tar -xvf sqlite-autoconf-3380500.tar.gz \
     &&	cd sqlite-autoconf-3380500 \
     && ./configure --prefix=$LIBDIR/sqlite3 \
     &&	make \
-    &&  make install
+    &&  make install \
+    &&  cd $SRCDIR
 
-rm -r -f $SRCDIR
+# Install vkl_lib
+git clone https://github.com/gvernard/vkl_lib.git \
+    &&	cd vkl_lib \
+    &&  autoreconf -i \
+    &&	./configure --prefix=$LIBDIR/vkl_lib/ --with-external=$LIBDIR/ \
+    &&  make \
+    &&  make install \
+    &&  cd $SRCDIR
+
+# Install gerlumphpp
+git clone https://github.com/gvernard/gerlumphpp.git \
+    &&	cd gerlumphpp \
+    &&  autoreconf -i \
+    &&  ./configure --prefix=$LIBDIR/gerlumphpp/ --with-map-path=$MAP_PATH --with-external=$LIBDIR/ --enable-gpu=$WITH_GPU \
+    &&  make \
+    &&  make install \
+    &&  cd $SRCDIR
+
+cd ..
+#rm -r -f $SRCDIR
