9eace358c02103a63a4f7b0db12588d9d08181db
gvernard <gvernard@astro.rug.nl>
2022-10-01 12:17:46 +0200

    Removed mask functions.

--- combine_light/include/mask_functions.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MASK_HPP
-#define MASK_HPP
-
-#include <string>
-#include "vkllib.hpp"
-
-void createMask(vkl::RectGrid* mydata,double smear,double threshold,std::string outfile);
-
-#endif /* MASK_HPP */
