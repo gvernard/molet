3a84c74643e1d43d51a915dc44d7f5c98fa0035e
gvernard <gvernard@astro.rug.nl>
2022-10-06 14:05:34 +0200

    Added support for the latest COOLEST standard and a test.

--- checks/src/coolest_conversion.cpp
+++ checks/src/coolest_conversion.cpp
@@ -5,7 +5,6 @@
 #include <vector>
 #include <dirent.h>
 #include <algorithm>
-#include <filesystem>
 
 #include "json/json.h"
 
@@ -18,7 +17,7 @@ Json::Value parseMass(Json::Value coolest_mass_model,std::string name){
     
     // Check each mass model parameter and convert its name and value as necessary
     std::string mmodel_type = coolest_mass_model[i]["type"].asString();
-    if( mmodel_type == "external_shear" ){
+    if( mmodel_type == "ExternalShear" ){
       profile["type"] = "external_shear";
       for( auto const& key : coolest_mass_model[i]["parameters"].getMemberNames()){
 	if( key == "gamma_ext" ){
@@ -60,7 +59,7 @@ Json::Value parseMass(Json::Value coolest_mass_model,std::string name){
 	}
       }
       
-    } else if( mmodel_type == "SPEMD" ){
+    } else if( mmodel_type == "PEMD" ){
       profile["type"] = "spemd";
       for( auto const& key : coolest_mass_model[i]["parameters"].getMemberNames()){
 	if( key == "center_x" ){
@@ -83,7 +82,27 @@ Json::Value parseMass(Json::Value coolest_mass_model,std::string name){
 	  fprintf(stderr,"Unknown parameter name '%s' for lensing entity '%s'!\n",key.c_str(),name.c_str());
 	}
       }
-	    
+
+    } else if( mmodel_type == "PixelatedRegularGrid" ){
+      profile["type"] = "pert";
+      for( auto const& key : coolest_mass_model[i]["pixels"].getMemberNames()){
+	if( key == "fits_file" ){
+	  profile["pars"]["filepath"] = coolest_mass_model[i]["pixels"][key].asString();
+	} else if( key == "field_of_view_x" ){
+	  profile["pars"]["xmin"] = coolest_mass_model[i]["pixels"][key][0].asDouble();
+	  profile["pars"]["xmax"] = coolest_mass_model[i]["pixels"][key][1].asDouble();
+	} else if( key == "field_of_view_y" ){
+	  profile["pars"]["ymin"] = coolest_mass_model[i]["pixels"][key][0].asDouble();
+	  profile["pars"]["ymax"] = coolest_mass_model[i]["pixels"][key][1].asDouble();
+	} else if( key == "num_pix_x" ){
+	  profile["pars"]["Nx"] = coolest_mass_model[i]["pixels"][key].asInt();
+	} else if( key == "num_pix_y" ){
+	  profile["pars"]["Ny"] = coolest_mass_model[i]["pixels"][key].asInt();
+	} else {
+	  fprintf(stderr,"Unknown parameter name '%s' for lensing entity '%s'!\n",key.c_str(),name.c_str());
+	}
+      }      
+
     } else {
       fprintf(stderr,"Unknown mass model type '%s' for lensing entity '%s'!\n",mmodel_type.c_str(),name.c_str());
     }
@@ -125,6 +144,27 @@ Json::Value parseLight(Json::Value coolest_light_model,std::string name){
 	  fprintf(stderr,"Unknown parameter name '%s' for lensing entity '%s'!\n",key.c_str(),name.c_str());
 	}
       }
+      
+    } else if( lmodel_type == "PixelatedRegularGrid" ){
+      profile["type"] = "custom";      
+      for( auto const& key : coolest_light_model[i]["pixels"].getMemberNames()){
+	if( key == "fits_file" ){
+	  profile["pars"]["filepath"] = coolest_light_model[i]["pixels"][key].asString();
+	} else if( key == "field_of_view_x" ){
+	  profile["pars"]["xmin"] = coolest_light_model[i]["pixels"][key][0].asDouble();
+	  profile["pars"]["xmax"] = coolest_light_model[i]["pixels"][key][1].asDouble();
+	} else if( key == "field_of_view_y" ){
+	  profile["pars"]["ymin"] = coolest_light_model[i]["pixels"][key][0].asDouble();
+	  profile["pars"]["ymax"] = coolest_light_model[i]["pixels"][key][1].asDouble();
+	} else if( key == "num_pix_x" ){
+	  profile["pars"]["Nx"] = coolest_light_model[i]["pixels"][key].asInt();
+	} else if( key == "num_pix_y" ){
+	  profile["pars"]["Ny"] = coolest_light_model[i]["pixels"][key].asInt();
+	} else {
+	  fprintf(stderr,"Unknown parameter name '%s' for lensing entity '%s'!\n",key.c_str(),name.c_str());
+	}
+      }
+      
     } else {
       fprintf(stderr,"Unknown light model type '%s' for lensing entity '%s'!\n",lmodel_type.c_str(),name.c_str());
     }
@@ -134,7 +174,6 @@ Json::Value parseLight(Json::Value coolest_light_model,std::string name){
 
   return profiles;
 }
-
 
 
       
@@ -153,7 +192,7 @@ int main(int argc,char* argv[]){
   
   // Check if the input file is in the coolest standard, if not then return.
   if( root.isMember("standard") ){
-    if( root["standard"] != "coolest" ){
+    if( root["standard"] != "COOLEST" ){
       fprintf(stdout,"No conversion required.\n");
       return 0;
     }
@@ -212,16 +251,20 @@ int main(int argc,char* argv[]){
     fprintf(stdout,"Instrument module '%s' already exists and will be used.\n",instrument_name.c_str());
     fprintf(stdout,"If you still want to provide it just rename the existing one in %s.\n",(molet_home+"instrument_modules/modules/").c_str());
   } else {
+
+    // Check if name contains forbitten characters, e.g. '/'
+    
+    fprintf(stdout,"Creating new instrument: '%s'\n",instrument_name.c_str());
     Json::Value new_instrument;
     new_instrument["name"] = root["instrument"]["name"];
     new_instrument["band"] = root["instrument"]["band"];
     new_instrument["readout"] = root["instrument"]["readout"];
     new_instrument["resolution"] = root["instrument"]["pixel_size"];
     Json::Value psf;
-    psf["pix_x"]  = root["instrument"]["psf"]["pix_x"];
-    psf["pix_y"]  = root["instrument"]["psf"]["pix_y"];
-    psf["width"]  = root["instrument"]["psf"]["width"];
-    psf["height"] = root["instrument"]["psf"]["height"];
+    psf["pix_x"]  = root["instrument"]["psf"]["pixels"]["num_pix_x"];
+    psf["pix_y"]  = root["instrument"]["psf"]["pixels"]["num_pix_y"];
+    psf["width"]  = root["instrument"]["psf"]["pixels"]["field_of_view_x"][1].asDouble() - root["instrument"]["psf"]["pixels"]["field_of_view_x"][0].asDouble();
+    psf["height"] = root["instrument"]["psf"]["pixels"]["field_of_view_y"][1].asDouble() - root["instrument"]["psf"]["pixels"]["field_of_view_y"][0].asDouble();
     new_instrument["psf"] = psf;
     Instrument::createNewInstrument(new_instrument,case_path+"psf.fits");
   }
@@ -231,28 +274,32 @@ int main(int argc,char* argv[]){
   Json::Value molet_instruments = Json::Value(Json::arrayValue);
   Json::Value instrument;
   instrument["name"] = instrument_name;
-  instrument["field-of-view_xmin"] = root["observation"]["fov-xmin"];
-  instrument["field-of-view_xmax"] = root["observation"]["fov-xmax"];
-  instrument["field-of-view_ymin"] = root["observation"]["fov-ymin"];
-  instrument["field-of-view_ymax"] = root["observation"]["fov-ymax"];
-  instrument["ZP"] = root["observation"]["ZP"];
+  instrument["field-of-view_xmin"] = root["observation"]["pixels"]["field_of_view_x"][0];
+  instrument["field-of-view_xmax"] = root["observation"]["pixels"]["field_of_view_x"][1];
+  instrument["field-of-view_ymin"] = root["observation"]["pixels"]["field_of_view_y"][0];
+  instrument["field-of-view_ymax"] = root["observation"]["pixels"]["field_of_view_y"][1];
+  instrument["ZP"] = root["observation"]["mag_zero_point"];
   std::string noise_type = root["observation"]["noise"]["type"].asString();
   Json::Value noise;
-  if( noise_type == "Poisson" ){
-    noise["type"] = "PoissonNoise";
-    noise["texp"] = root["observation"]["texp"];
-    noise["Msb"]  = root["observation"]["Msb"]; 
-  } else if( noise_type == "UniformGaussian" ){
+  if( noise_type == "InstrumentalNoise" ){
+    noise["type"] = "Poisson";
+    noise["texp"] = root["observation"]["exposure_time"];
+    noise["Msb"]  = root["observation"]["mag_sky_brightness"]; 
+  } else if( noise_type == "UniformGaussianNoise" ){
     noise["type"] = "UniformGaussian";
-    noise["sn"] = root["observation"]["noise"]["sn"];
+    noise["sn"] = root["observation"]["noise"]["std_dev"];
   } else if( noise_type == "NoiseMap" ){
     noise["type"] = "NoiseMap";
+  } else if( noise_type == "NoiseRealization" ){
+    noise["type"] = "NoiseRealization";
+    // something
   } else {
     fprintf(stderr,"Incompatible noise type '%s'!\n",noise_type.c_str());
   }
   instrument["noise"] = noise;
   molet_instruments.append( instrument );
 
+  
   
   // Loop over the lens planes and get the 'light' and 'mass' profiles of each 'lensing entity', while converting each profile to the MOLET conventions
   Json::Value molet_lenses = Json::Value(Json::arrayValue);
@@ -269,7 +316,6 @@ int main(int argc,char* argv[]){
     }
     lens["mass_model"] = mass_model;
 
-
     Json::Value light_model;
     light_model[instrument_name] = Json::Value(Json::arrayValue);
     for(int j=0;j<planes[i].size();j++){
@@ -280,7 +326,6 @@ int main(int argc,char* argv[]){
     }
     lens["light_profile"] = light_model;
 
-    
     molet_lenses.append( lens );
   }
     
@@ -306,9 +351,7 @@ int main(int argc,char* argv[]){
 
 
 
-
-  
-  // Create the file in the proper format acceptable by molet
+  // Create the file in the proper format acceptable by molet and write output
   Json::Value molet_final;
   molet_final["cosmology"] = molet_cosmo;
   molet_final["lenses"] = molet_lenses;
@@ -317,19 +360,15 @@ int main(int argc,char* argv[]){
   //std::cout << molet_final << std::endl;
   //std::cout << molet_instruments << std::endl;
 
-  // Create final molet input file
-  std::string fname = std::filesystem::path(input_file).filename().string();
-  std::filesystem::copy(input_file,case_path+"original_"+fname);
-  std::ofstream molet_input(input_file);
+  std::ofstream molet_input(case_path+"/molet_input.json");
   Json::StreamWriterBuilder wbuilder;
   wbuilder.settings_["precision"] = 6;
   wbuilder.settings_["indentation"] = "    ";
   std::unique_ptr<Json::StreamWriter> writer(wbuilder.newStreamWriter());
   writer->write(molet_final,&molet_input);
   molet_input.close();
+
+  
   
   return 0;
 }
-
-
-
